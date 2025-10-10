03d75fcdf9e86c5d205a9af7cfae3c8e6801303c
jeanr <jeanr@met.no>
2025-10-10 15:30:28 +0200

    Minor update

--- set_iptable_rules_denyall.sh
+++ set_iptable_rules_denyall.sh
@@ -2,6 +2,7 @@
 
 echo "Start set_iptable_rules_denyall.sh hook" > /home/jeanr/Desktop/Current/mylogs/log_set_iptable_rules_denyall.log 2>&1
 
+echo -n "whoami: "
 whoami >> /home/jeanr/Desktop/Current/mylogs/log_set_iptable_rules_denyall.log 2>&1
 
 # Block all incoming and outgoing traffic
@@ -11,6 +12,7 @@ iptables -P FORWARD DROP >> /home/jeanr/Desktop/Current/mylogs/log_set_iptable_r
 
 # Log the applied rules (optional, for debugging)
 echo "Applied iptables rules:" >> /home/jeanr/Desktop/Current/mylogs/log_set_iptable_rules_denyall.log 2>&1
+echo -n "iptables -L -v: "
 iptables -L -v >> /home/jeanr/Desktop/Current/mylogs/log_set_iptable_rules_denyall.log 2>&1
 
 echo "Finished set_iptable_rules_denayll.sh hook" >> /home/jeanr/Desktop/Current/mylogs/log_set_iptable_rules_denyall.log 2>&1
